b14d279a0f75148627098fad0e2a7b5c64fddce8
Daniel Addison <Daniel.addison1@marist.edu>
2017-09-14 16:53:22 -0400

    Made game, palyer score, player location, and implemented new levels

---
+++ Game.py
@@ -0,0 +1,52 @@
+# Introduction to Programming
+# Author: Daniel Addison
+# Date: 10/8/2017
+
+ghostrider = "A wooden rolercoater made to scare kids as if they were in a pirateship"   
+accelerator = " The fasteest ride at Knotts"
+boomerang = " Have you ever took a rolercoatrer ride and back again twice"
+funnelCakeStand = "Best funnel cake you would i buy at an amusment park"
+    
+print(" Knotts Scary Farm" )
+print("Get scared from your socks this october for a very special halloween")
+
+playerScore = 0
+playerLocation = funnelCakeStand
+
+print("player current score is " + str(playerScore ))
+print("player current Location is " + playerLocation)
+#move to ghostrider
+input("press enter to continue")
+
+playerLocation = ghostrider
+print("palyer current Location is " + playerLocation)
+
+playerScore = playerScore + 5
+print("player current score is " + str(playerScore ))
+#move to boomerang
+input("press enter to continue")
+
+playerLocation = boomerang
+print("palyer current Location is " + playerLocation)
+
+playerScore = playerScore + 5
+print("player current score is " + str(playerScore ))
+#move to accelerator
+input("press enter to continue")
+
+playerLocation = accelerator
+print("palyer current Location is " + playerLocation)
+
+playerScore = playerScore + 5
+print("player current score is " + str(playerScore ))
+#move to funnelCakestand
+input("press enter to continue")
+
+playerLocation = funnelCakeStand
+print("palyer current Location is " + playerLocation)
+
+playerScore = playerScore + 5
+print("player current score is " + str(playerScore ))
+
+print("You went home")
+print("copyright right 2017 Daniel Addison Daniel.addison1@marist.edu")
