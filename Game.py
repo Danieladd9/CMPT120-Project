55db1cc08ea5d5aa810061c58bb5c3d9f7c16a3d
Daniel Addison <Daniel.addison1@marist.edu>
2017-09-14 20:40:16 -0400

    Made game, player score, player location, and implemented new levels

--- Game.py
+++ Game.py
@@ -2,10 +2,10 @@
 # Author: Daniel Addison
 # Date: 10/8/2017
 
-ghostrider = "A wooden rolercoater made to scare kids as if they were in a pirateship"   
+ghostrider = "A wooden rollercoaster made to scare kids as if they were in a pirateship"   
 accelerator = " The fasteest ride at Knotts"
-boomerang = " Have you ever took a rolercoatrer ride and back again twice"
-funnelCakeStand = "Best funnel cake you would i buy at an amusment park"
+boomerang = " Have you ever took a rollercoaster ride and back again twice"
+funnelCakeStand = "Best funnel cake you would buy at an amusment park"
     
 print(" Knotts Scary Farm" )
 print("Get scared from your socks this october for a very special halloween")
@@ -19,7 +19,7 @@ print("player current Location is " + playerLocation)
 input("press enter to continue")
 
 playerLocation = ghostrider
-print("palyer current Location is " + playerLocation)
+print("player current Location is " + playerLocation)
 
 playerScore = playerScore + 5
 print("player current score is " + str(playerScore ))
@@ -27,7 +27,7 @@ print("player current score is " + str(playerScore ))
 input("press enter to continue")
 
 playerLocation = boomerang
-print("palyer current Location is " + playerLocation)
+print("player current Location is " + playerLocation)
 
 playerScore = playerScore + 5
 print("player current score is " + str(playerScore ))
@@ -35,7 +35,7 @@ print("player current score is " + str(playerScore ))
 input("press enter to continue")
 
 playerLocation = accelerator
-print("palyer current Location is " + playerLocation)
+print("player current Location is " + playerLocation)
 
 playerScore = playerScore + 5
 print("player current score is " + str(playerScore ))
@@ -43,10 +43,10 @@ print("player current score is " + str(playerScore ))
 input("press enter to continue")
 
 playerLocation = funnelCakeStand
-print("palyer current Location is " + playerLocation)
+print("player current Location is " + playerLocation)
 
 playerScore = playerScore + 5
 print("player current score is " + str(playerScore ))
 
 print("You went home")
-print("copyright right 2017 Daniel Addison Daniel.addison1@marist.edu")
+print("copyright 2017 Daniel Addison Daniel.addison1@marist.edu")
