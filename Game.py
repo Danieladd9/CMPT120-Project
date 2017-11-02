28a8fff7d09a0c10f927e2290a55d5c50b0d54ef
Daniel Addison <Daniel.addison1@marist.edu>
2017-11-02 12:48:53 -0400

    New version

--- Game.py
+++ Game.py
@@ -3,11 +3,11 @@
 # Date: 10/25/2017
 
 playerName = input("What is your name? ")
-print ("Hi " + playerName + " Welcome to Knotts Scary Farm")
+print ("Hi " + playerName + " Welcome to Knott’s Scary Farm")
 
 def printTitleIntro():
     print("Knotts Scary Farm")
-    print("Get scared from your socks this october for a very special night on halloween.")
+    print("Get scared from your socks this October for a very special night on Halloween.")
 
 printTitleIntro()
 
@@ -15,18 +15,34 @@ def printEndingCopyright():
     print("You went home after a long day of Fright Fest.")
     print("Copyright 2017 Daniel Addison Daniel.addison1@marist.edu")
 
+def gameloop():
+    print ("Game Loop")
+
+def changeLocation():
+    print ("Change Location")  
+
+#Locations
+ghostrider = "Ghostrider: A wooden rollercoaster, is the longest, tallest roller-coaster made to scare kids as if they were in a pirate ship"   
+xcelerator = "Xcelerator: This rocket looking roller-coaster, is the fastest ride at Knott’s going 0.82 mph in 2.3 sec"
+boomerang = "Boomerang: Have you ever took a rollercoaster ride and back again twice going 0 to 55 in 3 sec"
+funnelCakeStand = "The Funnel Stand: The Best funnel cake you would buy at an amusement park, you haven't had anything good till you tried it"
+bullet = "Bullet: Our new fastest Roller-coaster to date with VR technology for the experience"
+bathroom = "Bathroom: we understand that your so scared, but wait till you go to the bathroom for an even bigger surprise"
+shuttle = "Space Shuttle: Bus to carry you to the rides"
+cottonCandy = "Cotton Candy: right by funnel cake, best cotton candy you ever had"
+
 #new for project3  
-#this array holds the discription of the players location
-locationDiscription=["Ghostrider: A wooden rollercoaster, is the longet, \
-tallest rollercoater made to scare kids as if they were in a pirateship",
-                     "Xcelerator: This rocket looking rolercoaster, is the \
-fasteest ride at Knotts going 0.82 mph in 2.3 sec",
-                     "Boomarang: Have you ever took a rollercoaster ride and back again twice going 0 to 55 in 3 sec",
-                     "The Funnel Stand: The Best funnel cake you would buy at an amusment park, \
+#this array holds the description of the players location
+locationDiscription=["Ghostrider: A wooden rollercoaster, is the longest, \
+tallest roller-coaster made to scare kids as if they were in a pirate ship",
+                     "Xcelerator: This rocket looking roller-coaster, is the \
+fastest ride at Knott’s going 0.82 mph in 2.3 sec",
+                     "Boomerang: Have you ever took a rollercoaster ride and back again twice going 0 to 55 in 3 sec",
+                     "The Funnel Stand: The Best funnel cake you would buy at an amusement park, \
 you haven't had anything good till you tried it",
-                     "Bullet: Our new fastest RollerCoaster to date with VR technoligy for the experience",
+                     "Bullet: Our new fastest Roller-coaster to date with VR technology for the experience",
                      "Bathroom: we understand that your so scared,\
-but wait till you go to the bathroom for a even bigger suprise",
+but wait till you go to the bathroom for an even bigger surprise",
                      "Space Shuttle: Bus to carry you to the rides",
                      "Cotton Candy: right by funnel cake, best cotton candy you ever had"]                                        
 #newloc   
@@ -38,19 +54,23 @@ playerLocation = 3
 hasVisited=[False,False,False,True,False,False,False,False,False,False]
 
 #Time Limit
-playerMoves = 10
+playerMoves = 40
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 running = True
 while running:
-    #this checks if the player has any moves left
+    #this check if the player has any moves left
     if playerMoves == 0:
         running = False
     print("player current Location is " + locationDiscription[playerLocation])
-    playerInput = input("plaese enter a command: ").lower()
+    playerInput = input("please enter a command: ").lower()
     if playerInput == "north":
         print(playerName + " chose north")
         playerMoves -= 1
         if playerLocation == 0:
+            # move these fours lines into a new moveTo function
+            # where the parameter to the function is the location index
+            # then here just call that function with argument 1
+            #moveTo(1)
             if not hasVisited[1]:
                 playerScore = playerScore + 5
                 hasVisited[1] = True
@@ -81,7 +101,7 @@ while running:
                 hasVisited[7] = True
             playerLocation = 7
         else:
-            print("You can not go that way!")
+            print("You cannot go that way")
             
     elif playerInput == "south":
         print(playerName + " chose south")
@@ -117,7 +137,7 @@ while running:
                 hasVisited[5] = True
             playerLocation = 5
         else:
-            print("You can not go that way!")
+            print("You cannot go that way")
             
     elif playerInput == "east":
         print(playerName + " chose east")
@@ -143,7 +163,7 @@ while running:
                 hasVisited[6] = True
             playerLocation = 6
         else:
-            print("You can not go that way!")
+            print("You cannot go that way")
 
     elif playerInput == "west":
         print(playerName + " chose west")
@@ -169,13 +189,13 @@ while running:
                 hasVisited[7] = True
             playerLocation = 7
         else:
-            print("You can not go that way!")
+            print("You cannot go that way")
         
     elif playerInput == "help":
-        print("The commands to this game is: North, South, East, West, Help, and Quit")
+        print("The commands to this game is: North, South, East, West, Help, Points, Map, and Quit")
         playerMoves -= 1
     elif playerInput == "quit":
-        print("Goodbye,Thank You for playing")
+        print("Goodbye, Thank You for playing")
         running = False
     elif playerInput == "points":
         print("Player current score is " + str(playerScore))
