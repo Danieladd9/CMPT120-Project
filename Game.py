40746753cad74d28550d667a6806a9b076fa8a9d
Daniel Addison <Daniel.addison1@marist.edu>
2017-11-07 17:50:15 -0500

    changes for Project3, resolves #8.

--- Game.py
+++ Game.py
@@ -1,38 +1,10 @@
 # Introduction to Programming
 # Author: Daniel Addison
-# Date: 10/25/2017
-
-playerName = input("What is your name? ")
-print ("Hi " + playerName + " Welcome to Knott’s Scary Farm")
-
-def printTitleIntro():
-    print("Knotts Scary Farm")
-    print("Get scared from your socks this October for a very special night on Halloween.")
-
-printTitleIntro()
-
-def printEndingCopyright():
-    print("You went home after a long day of Fright Fest.")
-    print("Copyright 2017 Daniel Addison Daniel.addison1@marist.edu")
-
-def gameloop():
-    print ("Game Loop")
-
-def changeLocation():
-    print ("Change Location")  
-
-#Locations
-ghostrider = "Ghostrider: A wooden rollercoaster, is the longest, tallest roller-coaster made to scare kids as if they were in a pirate ship"   
-xcelerator = "Xcelerator: This rocket looking roller-coaster, is the fastest ride at Knott’s going 0.82 mph in 2.3 sec"
-boomerang = "Boomerang: Have you ever took a rollercoaster ride and back again twice going 0 to 55 in 3 sec"
-funnelCakeStand = "The Funnel Stand: The Best funnel cake you would buy at an amusement park, you haven't had anything good till you tried it"
-bullet = "Bullet: Our new fastest Roller-coaster to date with VR technology for the experience"
-bathroom = "Bathroom: we understand that your so scared, but wait till you go to the bathroom for an even bigger surprise"
-shuttle = "Space Shuttle: Bus to carry you to the rides"
-cottonCandy = "Cotton Candy: right by funnel cake, best cotton candy you ever had"
+# Date: 11/7/2017
 
 #new for project3  
 #this array holds the description of the players location
+#global need it for game
 locationDiscription=["Ghostrider: A wooden rollercoaster, is the longest, \
 tallest roller-coaster made to scare kids as if they were in a pirate ship",
                      "Xcelerator: This rocket looking roller-coaster, is the \
@@ -54,52 +26,71 @@ playerLocation = 3
 hasVisited=[False,False,False,True,False,False,False,False,False,False]
 
 #Time Limit
-playerMoves = 40
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-running = True
-while running:
+playerMoves = 20
+playerInput = 3
+playerName = ""
+def playerCustomization():
+    global playerName
+    playerName = input("What is your name? ")
+    print ("Hi " + playerName + " Welcome to Knott’s Scary Farm")
+
+def printTitleIntro():
+    print("Knott's Scary Farm")
+    print("=================\n")
+    print("Get scared from your socks this October for a very special night on Halloween.")
+
+def printEndingCopyright():
+    print("You went home after a long day of Fright Fest.")
+    print("Copyright 2017 Daniel Addison Daniel.addison1@marist.edu")
+    
+def initGameData():                                   
+    #newloc
+    global playerScore
+    playerScore = 5
+    global playerLocation
+    playerLocation = 3
+    
+    #Booleans
+    #this is how we know where the player has been
+    global hasVisited
+    hasVisited = [False,False,False,True,False,False,False,False,False,False]
+
+    #Time Limit
+    global playerMoves
+    playerMoves = 20
+    
+def showScene():
+    global locationDiscription
+    global playerLocation
+    print("player current Location is " + locationDiscription[playerLocation])
+    
+def processInput():
+    global playerInput
+    playerInput = input("please enter a command: ").lower()
+    
+def updateGame():
     #this check if the player has any moves left
+    global playerMoves
+    global playerName
+    global playerLocation
+    global playerInput
     if playerMoves == 0:
         running = False
-    print("player current Location is " + locationDiscription[playerLocation])
-    playerInput = input("please enter a command: ").lower()
     if playerInput == "north":
         print(playerName + " chose north")
         playerMoves -= 1
         if playerLocation == 0:
-            # move these fours lines into a new moveTo function
-            # where the parameter to the function is the location index
-            # then here just call that function with argument 1
-            #moveTo(1)
-            if not hasVisited[1]:
-                playerScore = playerScore + 5
-                hasVisited[1] = True
-            playerLocation = 1
+            moveTo(1)
         elif playerLocation == 1:
-            if not hasVisited[6]:
-                playerScore = playerScore + 5
-                hasVisited[6]= True
-            playerLocation = 6
+            moveTo(6)
         elif playerLocation == 4:
-            if not hasVisited[0]:
-                playerScore = playerScore + 5
-                hasVisited[0]= True
-            playerLocation = 0
+            moveTo(0)
         elif playerLocation == 3:
-            if not hasVisited[2]:
-                playerScore = playerScore + 5
-                hasVisited[2] = True
-            playerLocation = 2
+            moveTo(2)
         elif playerLocation == 2:
-            if not hasVisited[5]:
-                playerScore = playerScore + 5
-                hasVisited[5] = True
-            playerLocation = 5
+            moveTo(5)
         elif playerLocation == 5:
-            if not hasVisited[7]:
-                playerScore = playerScore + 5
-                hasVisited[7] = True
-            playerLocation = 7
+            moveTo(7)       
         else:
             print("You cannot go that way")
             
@@ -107,35 +98,17 @@ while running:
         print(playerName + " chose south")
         playerMoves -= 1
         if playerLocation == 1:
-            if not hasVisited[0]:
-                playerScore = playerScore + 5
-                hasVisited[0] = True
-            playerLocation = 0
+            moveTo(0)
         elif playerLocation == 0:
-            if not hasVisited[4]:
-                playerScore = playerScore + 5
-                hasVisited[4] = True
-            playerLocation = 4
+            moveTo(4)
         elif playerLocation == 5:
-            if not hasVisited[2]:
-                playerScore = playerScore + 5
-                hasVisited[2] = True
-            playerLocation = 2
+            moveTo(2)
         elif playerLocation == 2:
-            if not hasVisited[3]:
-                playerScore = playerScore + 5
-                hasVisited[3] = True
-            playerLocation = 3
+            moveTo(3)
         elif playerLocation == 6:
-            if not hasVisited[1]:
-                playerScore = playerScore + 5
-                hasVisited[1]= True
-            playerLocation = 1
+            moveTo(1)
         elif playerLocation == 7:
-            if not hasVisited[5]:
-                playerScore = playerScore + 5
-                hasVisited[5] = True
-            playerLocation = 5
+            moveTo(5)
         else:
             print("You cannot go that way")
             
@@ -143,25 +116,13 @@ while running:
         print(playerName + " chose east")
         playerMoves -= 1
         if playerLocation == 5:
-            if not hasVisited[1]:
-                playerScore = playerScore + 5
-                hasVisited[1] = True
-            playerLocation = 1
+            moveTo(1)
         elif playerLocation == 2:
-            if not hasVisited[0]:
-                playerScore = playerScore + 5
-                hasVisited[0] = True
-            playerLocation = 0
+            moveTo(0)
         elif playerLocation == 3:
-            if not hasVisited[4]:
-                playerScore = playerScore + 5
-                hasVisited[4] = True
-            playerLocation = 4
+            moveTo(4)
         elif playerLocation == 7:
-            if not hasVisited[6]:
-                playerScore = playerScore + 5
-                hasVisited[6] = True
-            playerLocation = 6
+            moveTo(6)
         else:
             print("You cannot go that way")
 
@@ -169,25 +130,13 @@ while running:
         print(playerName + " chose west")
         playerMoves -= 1
         if playerLocation == 1:
-            if not hasVisited[5]:
-                playerScore = playerScore + 5
-                hasVisited[5] = True
-            playerLocation = 5
+            moveTo(5)
         elif playerLocation == 0:
-            if not hasVisited[2]:
-                playerScore = playerScore + 5
-                hasVisited[2] = True
-            playerLocation = 2
+           moveTo(2)
         elif playerLocation == 4:
-            if not hasVisited[3]:
-                playerScore = playerScore + 5
-                hasVisited[3] = True
-            playerLocation = 3
+            moveTo(3)
         elif playerLocation == 6:
-            if not hasVisited[7]:
-                playerScore = playerScore + 5
-                hasVisited[7] = True
-            playerLocation = 7
+           moveTo(7)
         else:
             print("You cannot go that way")
         
@@ -196,7 +145,8 @@ while running:
         playerMoves -= 1
     elif playerInput == "quit":
         print("Goodbye, Thank You for playing")
-        running = False
+        printEndingCopyright()
+        quit()
     elif playerInput == "points":
         print("Player current score is " + str(playerScore))
         playerMoves -= 1
@@ -213,5 +163,26 @@ while running:
         print("2: Boomerang \n3: Funnelcake")
         print("4: Bullet \n5: Bathroom")
         print("6: Spaceshuttle \n7: Cottoncandy")
-        
-printEndingCopyright()
+    
+def moveTo(location):
+    global hasVisited
+    global playerScore
+    global playerLocation
+    if not hasVisited[location]:
+        playerScore = playerScore + 5
+        hasVisited[location] = True
+    playerLocation = location
+    
+def gameLoop():
+    running = True
+    while running:
+        showScene()
+        processInput()
+        updateGame()
+def main():
+    printTitleIntro()
+    playerCustomization()
+    initGameData()
+    gameLoop()
+    printEndingCopyright()
+main()
